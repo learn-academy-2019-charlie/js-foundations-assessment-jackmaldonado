43b93cc31310c82d6492beee0ed92ba677b79f66
EC2 Default User <ec2-user@ip-172-31-15-250.us-east-2.compute.internal>
2019-06-28 21:28:58 +0000

    finished assessment 2

--- js-foundations.js
+++ js-foundations.js
@@ -1,12 +1,43 @@
 // Javascript Foundations Assessments
-
 // 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
+//
+var divByThree = function (num){
+  if((num % 3) === 0){
+    return `${num} is divisible by three`
+  }else{
+    return `${num} is not divisible by three`
+  }
+}
 
+console.log(divByThree(10))
 // 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
+const helloMe = {
+  first_name: "Jack",
+  last_name: "Maldonado",
+  favSport: "Basketball",
+  favFood: "Steak",
+  getInfo: function(){
+    return `Hello I am ${this.first_name} ${this.last_name}, my favorite sport to watch is ${this.favSport} and I love ${this.favFood}`
+  }
+}
 
+console.log(helloMe.getInfo())
 // 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
 
+var grocerys = ["steak","eggs","chicken","fish","guac"]
+
+function shopping(arr){
+  return grocerys[0] + ", " + grocerys[Math.floor(grocerys.length/2)] + ", " + grocerys[grocerys.length - 1]
+
+}
+console.log(shopping(grocerys))
+
 // 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
+function alphabetSoup(str){
+  return str.split('').sort().join('');
+}
+console.log(alphabetSoup("learn"))
+
 
 // 5. Given the arrays below, write a function animalNums that uses a for loop to print one value from each array concatenated together.
 
@@ -14,6 +45,33 @@ var nums = [9, 5, 88, 2, 5, 42, 57]
 
 var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
 
+
+function animalNums(nums,nouns){
+  for(i = 0; i < Math.min(nums.length,nouns.length); i++){
+    console.log(nums[i] + " " + nouns[i])
+  }
+}
+animalNums(nums,nouns)
 // output should be: "9 ducks", etc
 
 //6. Create an array of 5 numbers. Write a function that loops through the array and returns a new array of the numbers multiplied by 5.
+
+var numbers = [1,2,3,4,5]
+
+function multFive(numbers){
+  var answer = []
+  for(i = 0; i < numbers.length; i++){
+    answer.push(numbers[i] * 5)
+  }
+  return answer
+}
+
+// function multFive(numbers){
+//   var answer = []
+//   numbers.forEach(element => {
+//     answer.push(element * 5)
+//   });
+//   return answer
+// }
+
+console.log(multFive(numbers))
--- js-foundations.md
+++ js-foundations.md
@@ -4,24 +4,44 @@
 
 1. List the data types in Javascript. Indicate which are primitives.
 
+arrays, strings, booleans, numbers and objects.
+primitive data types: 
+
 2. What's the difference between =, ==, and === in JavaScript?
+
+one equal sign is giving a value to something.
+three equal signs is comparing if something is equal to that value.
 
 3. What is the difference between an array's index and length?
 
+An arrays index is the position of the value.
+The length of in index is how long it is.
 4. What are the three parts of a for loop?
+
+i = x (tells us where the loop is starting)
+i <>= leangth (tells us how for the loop is supposed to go)
+i++ / i-- (tell us whether or not we are looping forward or backward)
 
 5. What is a function's declaration, argument, and call?
 
+The function declaration is the name if the function. The argument is what you are going to passs down throughout the condition and the call is what you are going to test to make sure it runs.
 6. What are the three main steps in saving work to github?
 
+git add . 
+git commit -m "what you changed"
+git push
 7. What is the terminal command to move directories?
 
+cd
 8. Do you have a suggestion for a Check In question?
 
+Read the question when you first get to class so you can think about it.
 9. What was your favorite topic this week?
 
+I really enjoy React, just wish I understood it more.
 10. What was your "A-ha!" moment this week?
 
+When I realized how much I had to study in order to be successful.
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
 1. Look at this Javascript and try to predict, what will the browser's console show?
@@ -34,8 +54,9 @@ function logIt(){
 }
 
 logIt()
-```
 
+"outside"
 2. What is JSON? How does it relate to javascript objects?
+JSON is a way to store data and is very silimar ro objects in javascript
 
 3. Describe a closure, what is it good for and how do you recognize one?
